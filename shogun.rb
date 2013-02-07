7e15b5fde6e773b6bf687f20bf584b98290d4a79
Ben Acland <benacland@gmail.com>
2013-02-07 16:00:37 -0600

    pkg-config is a build dependency.

--- shogun.rb
+++ shogun.rb
@@ -5,7 +5,7 @@ class Shogun < Formula
   url 'http://shogun-toolbox.org/archives/shogun/releases/2.0/sources/shogun-2.0.0.tar.bz2'
   sha1 '4077b520c07669caf77f4c92e0fa6e5955dc929c'
 
-  depends_on 'pkg-config'
+  depends_on 'pkg-config' => :build
   depends_on 'r' => :optional
   depends_on 'lua' => :optional
 
